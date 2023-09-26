f315f9f74c46f1adff49c778acc764d3cdeaf644
saanalapinkangas <118981440+saanalapinkangas@users.noreply.github.com>
2023-09-26 10:43:13 +0300

    ankkurointi

--- assets/stuff.js
+++ assets/stuff.js
@@ -59,3 +59,29 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+// ankkurointi
+// Etsi kaikki linkit, jotka viittaavat ankkureihin (#) ja lisää niihin vierityksen käsittelijä
+document.addEventListener("DOMContentLoaded", function () {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        // Hae kohde-elementti ankkurista
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            // Laske kohteen sijainti sivulla
+            const targetOffset = targetElement.getBoundingClientRect().top;
+
+            // Vieritä sivu haluttuun kohtaan
+            window.scrollTo({
+                top: window.scrollY + targetOffset,
+                behavior: 'smooth' // Tämä luo sileän vierityksen
+            });
+        }
+    });
+});
+});
