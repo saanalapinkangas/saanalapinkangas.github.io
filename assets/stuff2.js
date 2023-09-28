ebdbad25102a9408f35289f873b6640bfa43268b
saanalapinkangas <118981440+saanalapinkangas@users.noreply.github.com>
2023-09-28 15:24:40 +0300

    käännösnappi

--- assets/stuff2.js
+++ assets/stuff2.js
@@ -1,12 +1,26 @@
-// edellinen ja seuraava
+// Haetaan tarvittavat elementit DOM:ista
 document.addEventListener("DOMContentLoaded", function () {
-    const button = document.getElementById('custom-button');
-    
-    button.addEventListener('mouseover', () => {
-        button.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(dark-background.jpg)';
-    });
-    
-    button.addEventListener('mouseout', () => {
-        button.style.backgroundImage = '';
-    });
-    });
+
+    // Haetaan tarvittavat elementit DOM:ista
+const vanhaTekstiElementti = document.getElementById("vanhaTeksti");
+const uusiTekstiElementti = document.getElementById("uusiTeksti");
+const vaihdaNappi = document.getElementById("vaihdaNappi");
+
+// Lisätään nappiin klikkikuuntelija
+vaihdaNappi.addEventListener("click", () => {
+    // Tarkistetaan, kumpi teksti näytetään ja kumpi piilotetaan
+    if (vanhaTekstiElementti.classList.contains("nayta")) {
+        vanhaTekstiElementti.classList.remove("nayta");
+        vanhaTekstiElementti.classList.add("piilossa");
+        uusiTekstiElementti.classList.remove("piilossa");
+        uusiTekstiElementti.classList.add("nayta");
+    } else {
+        uusiTekstiElementti.classList.remove("nayta");
+        uusiTekstiElementti.classList.add("piilossa");
+        vanhaTekstiElementti.classList.remove("piilossa");
+        vanhaTekstiElementti.classList.add("nayta");
+    }
+});
+
+
+});
