0c2417026973918fa3815dc1ed23f20f39002c33
saanalapinkangas <118981440+saanalapinkangas@users.noreply.github.com>
2023-09-28 14:43:47 +0300

    responsiivisuus ok

--- assets/stuff.js
+++ assets/stuff.js
@@ -55,21 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
             navigaatio.style.top = "0";
         } else {
             // Piilota navigaatiopalkki
-            navigaatio.style.top = "-70px";
+            navigaatio.style.top = "-90px";
         }
     });
-});
-
-
-// edellinen ja seuraava
-document.addEventListener("DOMContentLoaded", function () {
-const button = document.getElementById('custom-button');
-
-button.addEventListener('mouseover', () => {
-    button.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(dark-background.jpg)';
-});
-
-button.addEventListener('mouseout', () => {
-    button.style.backgroundImage = '';
-});
 });
